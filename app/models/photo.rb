d776ef73e133aea77b7714235ebf925a55c299af
Sabrina Ooi <heysabs@gmail.com>
2016-10-05 12:12:01 +0800

    testing

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :item
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, :path => ":rails_root/public/photos/:id/:filename", :url => "/photos/:id/:filename"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
